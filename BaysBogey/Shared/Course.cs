36dcb5047a1c356bd95afe5f6dff03e6699b2c2d
Killerwalski <pakowalski86@gmail.com>
2020-09-30 18:47:35 -0400

    Update packages, add models

---
+++ BaysBogey/Shared/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaysBogey.Shared
+{
+    public class Course
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<Hole> Holes { get; set; }
+    }
+}
---
+++ BaysBogey/Shared/Hole.cs
@@ -0,0 +1,11 @@
+﻿using AspNetMonsters.Blazor.Geolocation;
+using System.Collections.Generic;
+
+namespace BaysBogey.Shared
+{
+    public class Hole
+    {
+        public Dictionary<string, Location> TeeBoxes { get; set; }
+        public Location Pin { get; set; }
+    }
+}
