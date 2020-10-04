1b71d4297da427dbd153a4f466ab1847fa01ba88
Killerwalski <pakowalski86@gmail.com>
2020-10-04 00:20:51 -0400

    Mongodb

--- BaysBogey.Tests/DataTests.cs
+++ BaysBogey.Tests/DataTests.cs
@@ -1,6 +1,8 @@
 using BaysBogey.Server.Services;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -11,12 +13,25 @@ namespace BaysBogey.Tests
     {
         private ILogger Logger;
         private IDataService DataService;
+        public IConfiguration Configuration;
+
         public DataTests(ITestOutputHelper output)
         {
-            DataService = new FakeDataService();
+            Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.TestOutput(output, Serilog.Events.LogEventLevel.Verbose)
+                .CreateLogger()
+                .ForContext<DataTests>();
+
+            Configuration = new ConfigurationBuilder()
+                // .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName + @"\BaysBogey.Server\appsettings.Development.json", false, true)
+                .Build();
+
+            DataService = new BaysBogeyDataService(Configuration, Logger);
         }
         [Fact]
-        public async Task FakeDataService_Gets_Course()
+        public async Task DataService_Gets_Course()
         {
             var course = await DataService.GetCourse("hi");
             Assert.NotNull(course);
--- BaysBogey/Server/Services/BaysBogeyDataService.cs
+++ BaysBogey/Server/Services/BaysBogeyDataService.cs
@@ -1,4 +1,7 @@
 ﻿using BaysBogey.Shared;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,23 @@ namespace BaysBogey.Server.Services
 {
     public class BaysBogeyDataService : IDataService
     {
+        private IConfiguration Configuration;
+        private IMongoCollection<Course> CourseCollection;
+        public ILogger Logger;
+
+        public BaysBogeyDataService(IConfiguration configuration, ILogger logger)
+        {
+            Configuration = configuration;
+            Logger = logger;
+
+            var connectionString = Configuration["BrawlhallaDatabaseSettings:ConnectionString"];
+            var databaseName = Configuration["BrawlhallaDatabaseSettings:DatabaseName"];
+            var collectionName = Configuration["BrawlhallaDatabaseSettings:CollectionName"];
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            CourseCollection = database.GetCollection<Course>(collectionName);
+        }
         public void AddCourse(Course course)
         {
             
@@ -15,7 +35,9 @@ namespace BaysBogey.Server.Services
 
         public async Task<Course> GetCourse(string id)
         {
-            return null;
+            var filter = Builders<Course>.Filter.Eq(c => c.Id == id, true);
+            var course = await CourseCollection.FindAsync(filter);
+            return await course.FirstOrDefaultAsync();
         }
 
         public Task<IEnumerable<Course>> GetCourses()
